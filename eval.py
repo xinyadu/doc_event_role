483bf1f86363b97505b7ad054f68f7c457718aa8
Xinya Du <louis.xyd@gmail.com>
2020-07-05 12:33:42 -0400

    update eval.py

--- eval.py
+++ eval.py
@@ -12,9 +12,9 @@ from collections import OrderedDict
 
 import spacy
 nlp = spacy.load("en_core_web_sm") # for finding head noun
-from spacy.tokenizer import Tokenizer
-from spacy.lang.en import English
-tokenizer = Tokenizer(nlp.vocab)
+# from spacy.tokenizer import Tokenizer
+# from spacy.lang.en import English
+# tokenizer = Tokenizer(nlp.vocab)
 
 tag2name = OrderedDict([('perp_individual_id', "PerpInd"), ('perp_organization_id',"PerpOrg"), ('phys_tgt_id',"Target"), ('hum_tgt_name',"Victim"), ('incident_instrument_id',"Weapon")])
 
@@ -226,179 +226,6 @@ def eval_head_noun(doc_pred_mentions, doc_gold_entitys):
 
 
 
-
-def get_eval_results(doc_pred_spans, doc_gold_events, to_print=False):
-    ## get pred spans (tokenized and headnoun)
-    doc_pred_spans_head_noun = dict()
-    for doc_id in doc_pred_spans:
-        doc_pred_spans_head_noun[doc_id] = dict()
-        for tag in tag2name:
-            doc_pred_spans_head_noun[doc_id][tag] = list()
-            for idx, span in enumerate(doc_pred_spans[doc_id][tag]):
-                # span_tokenized = tokenizer.tokenize(span) # to normalize (remove other diff between pred and gold)
-                span_tokenized = tokenizer(span.lower()).text
-                doc_pred_spans[doc_id][tag][idx] = span_tokenized
-
-                head_noun = list()
-                # noun_chunks = list(nlp(" ".join(span_tokenized).replace(" ##", '')).noun_chunks)
-                noun_chunks = list(nlp(span.lower()).noun_chunks)
-                for noun_chunk in noun_chunks: 
-                    head_noun.append(noun_chunk.root.text)
-
-                doc_pred_spans_head_noun[doc_id][tag].append({"span": span_tokenized, "hn": head_noun})
-
-            doc_pred_spans[doc_id][tag] = remove_duplicate(doc_pred_spans[doc_id][tag])
-            doc_pred_spans_head_noun[doc_id][tag] = remove_duplicate_head_noun(doc_pred_spans_head_noun[doc_id][tag])
-
-
-    ## get gold event (tokenized and headnoun)
-    doc_gold_events_exact = dict()
-    doc_gold_events_head_noun = dict()
-    for doc_id in doc_gold_events:
-        doc_gold_events_exact[doc_id] = dict()
-        doc_gold_events_head_noun[doc_id] = dict()
-        for tag in tag2name:
-            doc_gold_events_exact[doc_id][tag] = list()
-            doc_gold_events_head_noun[doc_id][tag] = list()
-            for event in doc_gold_events[doc_id]["roles"][tag]:
-                event_tokenized = []
-                event_head_noun = []
-
-                for span in event:
-                    # span_tokenized = tokenizer.tokenize(span)
-                    span_tokenized = tokenizer(span.lower()).text
-                    event_tokenized.append(span_tokenized)
-
-                    head_noun = list()
-                    # noun_chunks = list(nlp(" ".join(span_tokenized).replace(" ##", '')).noun_chunks)
-                    noun_chunks = list(nlp(span.lower()).noun_chunks)
-                    for noun_chunk in noun_chunks: 
-                        head_noun.append(noun_chunk.root.text)
-                    event_head_noun.append({"span": span_tokenized, "hn": head_noun})
-
-                doc_gold_events_exact[doc_id][tag].append(event_tokenized)
-                doc_gold_events_head_noun[doc_id][tag].append(event_head_noun)
-
-    
-    ##
-    ## report exact
-    ##
-    prec_macro, recall_macro = 0, 0
-    if to_print:
-        print("Exact Match\n", "precision, recall, F-1")
-    final_print = []
-    for tag in tag2name:
-        gold_event_num, right_event_num, pred_span_num, right_span_num = 0, 0, 0, 0
-        for doc_id in doc_gold_events_exact:
-            gold_events = doc_gold_events_exact[doc_id][tag]
-            if doc_id not in doc_pred_spans: continue
-            pred_spans = doc_pred_spans[doc_id][tag]
-
-            # for recall
-            for event in gold_events:
-                gold_event_num += 1
-                if match_exact(pred_spans, event):
-                    right_event_num += 1
-            # for prec
-            for span in pred_spans:
-                pred_span_num += 1
-                all_events = list()
-                for event in gold_events:
-                    all_events += event
-                if match_exact([span], all_events):
-                    right_span_num += 1
-
-        recall, prec = -1, -1
-        if gold_event_num: recall = (right_event_num + 0.0) / gold_event_num
-        if pred_span_num: prec = (right_span_num + 0.0) / pred_span_num
-
-        if prec <= 0 or recall <= 0:
-            f_measure = -1
-        else:
-            prec *= 100
-            recall *= 100
-            f_measure = 2*prec*recall/(prec+recall)
-
-        prec_macro += prec
-        recall_macro += recall
-
-        if to_print:
-            print("%s\n%.4f %.4f %.4f"%(tag2name[tag], prec, recall, f_measure))
-        final_print += [prec, recall, f_measure]
-
-    prec_macro = prec_macro / 5
-    recall_macro = recall_macro / 5
-    f_measure_macro = 2*prec_macro*recall_macro/(prec_macro+recall_macro)
-    final_print += [prec_macro, recall_macro, f_measure_macro]
-    # print("\n\nmacro avg")
-    if to_print:
-        print("Macro:")
-        print("%.4f %.4f %.4f"%(prec_macro, recall_macro, f_measure_macro))
-        print("\nfinal_print")
-        for num in final_print: print(num, end = " ")
-
-
-    ##
-    ## report head noun
-    ##
-    doc_pred_spans, doc_gold_events = doc_pred_spans_head_noun, doc_gold_events_head_noun
-
-    prec_macro, recall_macro = 0, 0
-    if to_print:
-        print("\n\nHead Noun Match\n", "precision, recall, F-1")
-    final_print = []
-    for tag in tag2name:
-        gold_event_num, right_event_num, pred_span_num, right_span_num = 0, 0, 0, 0
-        for doc_id in doc_gold_events:
-            gold_events = doc_gold_events[doc_id][tag]
-            if doc_id not in doc_pred_spans: continue
-            pred_spans = doc_pred_spans[doc_id][tag]
-
-            # for recall
-            for event in gold_events:
-                gold_event_num += 1
-                if match_head_noun(pred_spans, event):
-                    right_event_num += 1
-            # for prec
-            for span in pred_spans:
-                pred_span_num += 1
-                all_events = list()
-                for event in gold_events:
-                    all_events += event
-                if match_head_noun([span], all_events):
-                    right_span_num += 1
-
-        recall, prec = -1, -1
-        if gold_event_num: recall = (right_event_num + 0.0) / gold_event_num
-        if pred_span_num: prec = (right_span_num + 0.0) / pred_span_num
-
-        if prec <= 0 or recall <= 0:
-            f_measure = -1
-        else:
-            prec *= 100
-            recall *= 100
-            f_measure = 2*prec*recall/(prec+recall)
-
-        prec_macro += prec
-        recall_macro += recall
-
-        if to_print:
-            print("%s\n%.4f %.4f %.4f"%(tag2name[tag], prec, recall, f_measure))
-        final_print += [prec, recall, f_measure]
-
-    prec_macro = prec_macro / 5
-    recall_macro = recall_macro / 5
-    f_measure_macro = 2*prec_macro*recall_macro/(prec_macro+recall_macro)
-    final_print += [prec_macro, recall_macro, f_measure_macro]
-    if to_print:
-        print("Macro:")
-        print("%.4f %.4f %.4f"%(prec_macro, recall_macro, f_measure_macro))
-        print("\nfinal_print")
-        for num in final_print: print(num, end = " ")
-
-    return prec_macro, recall_macro, f_measure_macro
-
-
 if __name__ == '__main__':
     p = argparse.ArgumentParser()
     p.add_argument('--predfile')
@@ -413,7 +240,7 @@ if __name__ == '__main__':
         doc_gold_entitys = json.loads(f_gold.read())     
 
     # # obtain eval results
-    # eval_exact(copy.deepcopy(doc_pred_mentions), copy.deepcopy(doc_gold_entitys))
-    # eval_head_noun(copy.deepcopy(doc_pred_mentions), copy.deepcopy(doc_gold_entitys))
+    eval_exact(copy.deepcopy(doc_pred_mentions), copy.deepcopy(doc_gold_entitys))
+    eval_head_noun(copy.deepcopy(doc_pred_mentions), copy.deepcopy(doc_gold_entitys))
 
-    get_eval_results(doc_pred_mentions, doc_gold_entitys, to_print=True)
+    # get_eval_results(doc_pred_mentions, doc_gold_entitys, to_print=True)
